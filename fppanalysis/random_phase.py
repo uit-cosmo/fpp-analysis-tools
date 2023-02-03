c247774bc5f0568215cc5f0a2434af33ff7fcf88
Audun Theodorsen <audun.theodorsen@uit.no>
2023-02-03 12:52:04 +0100

    bugfix in random phases

--- fppanalysis/random_phase.py
+++ fppanalysis/random_phase.py
@@ -18,6 +18,13 @@ def signal_rand_phase(S):
     F = np.fft.rfft(S)
 
     pf = rand_phase(np.angle(F))
+    # See np.fft.rfft: The zero frequency must be real,
+    # as must the last freqency if there is an even number
+    # of samples.
+    pf[0] = 0
+    if not S.size%2:
+        pf[-1] = 0
     Ff = np.abs(F) * np.exp(1.0j * pf)
-
-    return np.fft.irfft(Ff)
+    
+    # Specify length of inverse transform to correctly deal with odd samples
+    return np.fft.irfft(Ff,len(S))
