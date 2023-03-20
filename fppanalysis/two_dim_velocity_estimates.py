29915c946d81602436183e386dfa98ead407e761
Aurora Driveklepp Helgeland <aurora.helgeland@uit.no>
2023-03-20 14:03:20 +0100

    specify either cross correlation or cross conditional average as input argument

--- fppanalysis/two_dim_velocity_estimates.py
+++ fppanalysis/two_dim_velocity_estimates.py
@@ -77,7 +77,7 @@ def _get_dt(ds):
 
 
 
-def _estimate_velocities_given_points(p0, p1, p2, ds):
+def _estimate_velocities_given_points(p0, p1, p2, ds, cross_cond_av: bool):
     dt = _get_dt(ds)
     r0, z0 = _get_rz(p0[0], p0[1], ds)
     r1, z1 = _get_rz(p1[0], p1[1], ds)
@@ -89,9 +89,13 @@ def _estimate_velocities_given_points(p0, p1, p2, ds):
     if len(signal0) == 0 or len(signal1) == 0 or len(signal2) == 0:
         return None
     
-
-    delta_tx, cx = tde.estimate_time_delay_ccond_av_max(signal1, signal1.time.values, signal0, signal0.time.values)
-    delta_ty, cy = tde.estimate_time_delay_ccond_av_max(signal2, signal2.time.values, signal0, signal0.time.values)
+    if cross_cond_av:
+        delta_tx, cx = tde.estimate_time_delay_ccond_av_max(signal1, signal1.time.values, signal0, signal0.time.values)
+        delta_ty, cy = tde.estimate_time_delay_ccond_av_max(signal2, signal2.time.values, signal0, signal0.time.values)
+    else: 
+        delta_ty, cy = tde.estimate_time_delay_ccmax(x=signal2, y=signal0, dt=dt)
+        delta_tx, cx = tde.estimate_time_delay_ccmax(x=signal1, y=signal0, dt=dt)
+        
     confidence = min(cx, cy)
 
     return (
@@ -104,7 +108,7 @@ def _is_within_boundaries(p, ds):
     return 0 <= p[0] < ds.sizes["x"] and 0 <= p[1] < ds.sizes["y"]
 
 
-def estimate_velocities_for_pixel(x, y, ds: xr.Dataset):
+def estimate_velocities_for_pixel(x, y, ds: xr.Dataset, cross_cond_av: bool):
     """Estimates radial and poloidal velocity for a pixel with indexes x,y
     using all four possible combinations of nearest neighbour pixels (x-1, y),
     (x, y+1), (x+1, y) and (x, y-1). Dead-pixels (stored as np.nan arrays) are
@@ -124,7 +128,7 @@ def estimate_velocities_for_pixel(x, y, ds: xr.Dataset):
     h_neighbors = [(x - 1, y), (x + 1, y)]
     v_neighbors = [(x, y - 1), (x, y + 1)]
     results = [
-        _estimate_velocities_given_points((x, y), px, py, ds)
+        _estimate_velocities_given_points((x, y), px, py, ds, cross_cond_av)
         for px in h_neighbors
         if _is_within_boundaries(px, ds)
         for py in v_neighbors
@@ -139,13 +143,17 @@ def estimate_velocities_for_pixel(x, y, ds: xr.Dataset):
     return mean_vx, mean_vy, confidence
 
 
-def estimate_velocity_field(ds: xr.Dataset):
+def estimate_velocity_field(ds: xr.Dataset, cross_cond_av: bool):
     """
     Given a dataset ds with GPI data in a format produced by https://github.com/sajidah-ahmed/cmod_functions,
     computed the velocity field. The estimation takes into account poloidal flows as described in
     the 2D filament model. For each pixel, the velocities are estimated using the given pixel, and two neighbour
     pixels: the right neighbour and the down neighbour. The return objects are matrices of the size of the
     GPI grid, from which the velocity field can be easily plotted via f.e matplotlib.quiver.
+
+    Input:
+        To estimate velocity field of cross conditional average then cross_cond_av = True
+        To estimate velocity field of cross correlation then cross_cond_av = False
 
     Returns:
         vx Radial velocities
@@ -164,7 +172,7 @@ def estimate_velocity_field(ds: xr.Dataset):
         for j in range(0, shape[1]):
             try:
                 vx[i, j], vy[i, j], confidences[i, j] = estimate_velocities_for_pixel(
-                    i, j, ds
+                    i, j, ds, cross_cond_av,
                 )
             except:
                 print(
